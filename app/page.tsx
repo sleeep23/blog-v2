8f230527d215f0980f3f54b26cdaf8e7a6ff8a0c
Dongho Seo <ehcws333@gmail.com>
2023-10-17 03:09:36 +0900

    chore: add date to each posts

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
-import { allPosts } from "@/.contentlayer/generated";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import Link from "next/link";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { allPosts } from "@/.contentlayer/generated";
+import { compareDesc, format, parseISO } from "date-fns";
 
 export default function Home() {
   return (
@@ -27,16 +28,23 @@ export default function Home() {
         </div>
       </section>
       <section id="posts">
-        <h1 className="text-2xl font-bold">Posts</h1>
+        <h3 className="text-2xl font-bold font-mono">Posts</h3>
         {allPosts.map((post) => (
           <article key={post._id} className="mt-4">
             <Link
               href={post.slug}
-              className="no-underline hover:underline underline-offset-2 decoration-cyan-500 decoration-2"
+              className="text-sm no-underline hover:underline underline-offset-2 decoration-cyan-500 decoration-2"
             >
               <h3 className="font-medium mb-0.5">{post.title}</h3>
               {post.description && (
-                <p className="font-light text-gray-500">{post.description}</p>
+                <p className="font-medium text-gray-500 mb-2">
+                  {post.description}
+                </p>
+              )}
+              {post.date && (
+                <p className="text-xs mt-0">
+                  {format(parseISO(post.date), "LLLL d, yyyy")}
+                </p>
               )}
             </Link>
           </article>
